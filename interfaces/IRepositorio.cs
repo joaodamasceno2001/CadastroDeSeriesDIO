b535cd695ffc0877f7e2f5935de4d11e6c28036e
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-09-06 23:58:39 -0300

    Projeto DIO

---
+++ interfaces/IRepositorio.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace DIO.Series.interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+
+        T RetornaPorId(int id);
+
+        void Insere(T entidade);
+
+        void Excluir(int id);
+
+        void Atualizar(int id, T entidade);
+
+        int ProximoId();
+
+    }
+}
